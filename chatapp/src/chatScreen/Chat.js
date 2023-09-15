ff08b21b6918e55e07a610ab05ff22e740e00c0d
Vineet Agrawal <vineetagrawal745@gmail.com>
2023-09-15 14:48:54 +0530

    final commit

--- chatapp/src/chatScreen/Chat.js
+++ chatapp/src/chatScreen/Chat.js
@@ -60,71 +60,50 @@ const Chat = ({route}) => {
     setMessage('');
   };
   return (
-    // <View></View>
-    <View style={{flex: 1}}>
-      <View style={{flex: 1}}>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 1 }}>
         {allmessages.map((msg, index) => (
-          <View key={index}>
-            {msg.receiverId === user._id ? (
-              <View
-                style={{
-                  borderWidth: 1,
-                  borderRadius: 10,
-                  height: 50,
-                  marginVertical: 10,
-                  justifyContent: 'center',
-                  paddingHorizontal: 10,
-                  width: 280,
-                }}>
-                <Text style={{color: 'black', fontSize: 18}}>{msg.text}</Text>
-              </View>
-            ) : (
-              <View
-                style={{
-                  borderWidth: 1,
-                  borderRadius: 10,
-                  height: 50,
-                  marginVertical: 10,
-                  justifyContent: 'center',
-                  paddingHorizontal: 10,
-                  width: 280,
-                  position: 'absolute',
-                  right: 0,
-                }}>
-                <Text style={{color: 'black', fontSize: 18}}>{msg.text}</Text>
-              </View>
-            )}
+          <View key={index} style={msg.receiverId === user._id ? styles.leftMessage : styles.rightMessage}>
+            <Text style={{ color: 'black', fontSize: 18 }}>{msg.text}</Text>
           </View>
         ))}
       </View>
-      <View
-        style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          paddingHorizontal: 10,
-        }}>
+      <View style={styles.inputContainer}>
         <TextInput
-          style={{borderWidth: 1, width: '80%'}}
+          style={styles.input}
           value={message}
           onChangeText={value => setMessage(value)}
           placeholder="Type Message"
         />
-        <TouchableOpacity
-          style={{
-            backgroundColor: 'lightblue',
-            width: 60,
-            height: 50,
-            alignItems: 'center',
-            justifyContent: 'center',
-            borderRadius: 10,
-          }}
-          onPress={sendMessage}>
+        <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
           <Text>Send</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
+};
+
+const styles = {
+  leftMessage: {
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 50,
+    marginVertical: 10,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    width: 280,
+  },
+  rightMessage: {
+    borderWidth: 1,
+    borderRadius: 10,
+    height: 50,
+    marginVertical: 10,
+    justifyContent: 'center',
+    paddingHorizontal: 10,
+    width: 280,
+    position: 'absolute',
+    right: 0,
+  },
 };
 
 export default Chat;
